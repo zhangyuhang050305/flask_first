9e257f7990e530e7c4bed05a9b6a425cd1af4184
zhangyuhang050305 <1507650947@qq.com>
2026-05-19 19:58:19 +0800

    网站logo显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,5 +1,5 @@
 import logging
-from flask import current_app
+from flask import current_app,render_template
 
 from info import redis_store
 from info.modules.index import index_blue
@@ -15,15 +15,20 @@ def index():
     #print(session.get('name'))
 
     # 使用日志记录方法loggin进行输出可控
-    logging.debug('输入调试信息')
-    logging.info('输入详细信息')
-    logging.warning('输入警告信息')
-    logging.error('输入错误信息')
+    # logging.debug('输入调试信息')
+    # logging.info('输入详细信息')
+    # logging.warning('输入警告信息')
+    # logging.error('输入错误信息')
+    #
+    # # 使用current_app进行输出可控
+    # current_app.logger.debug('输入调试信息')
+    # current_app.logger.info('输入详细信息')
+    # current_app.logger.warning('输入警告信息')
+    # current_app.logger.error('输入错误信息')
 
-    # 使用current_app进行输出可控
-    current_app.logger.debug('输入调试信息')
-    current_app.logger.info('输入详细信息')
-    current_app.logger.warning('输入警告信息')
-    current_app.logger.error('输入错误信息')
+    return render_template("news/index.html")
 
-    return 'hello world'
+# 处理网站logo
+@index_blue.route('/favicon.ico')
+def get_web_logo():
+    return current_app.send_static_file('news/favicon.ico')
