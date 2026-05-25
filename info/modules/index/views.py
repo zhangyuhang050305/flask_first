13111de86af62d1d771cf041dacbc54d245d6295
zhangyuhang050305 <1507650947@qq.com>
2026-05-25 21:19:15 +0800

    首页用户显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,32 +1,29 @@
 import logging
-from flask import current_app,render_template
+from flask import current_app, render_template, session
 
 from info import redis_store
 from info.modules.index import index_blue
+from info.models import User
 
-@index_blue.route('/', methods=['GET', 'POST'])
+@index_blue.route('/',methods=['GET','POST'])
 def index():
-    # 测试redis存取数据
-    # redis_store.set('name','laowang')
-    # print(redis_store.get('name'))
+    # 1.获取用户的登录信息
+    user_id = session.get('user_id')
 
-    # 测试session存取
-    #session['name'] = 'zhangsan'
-    #print(session.get('name'))
+    # 2.通过user_id取出用户的对象
+    user =None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
 
-    # 使用日志记录方法loggin进行输出可控
-    # logging.debug('输入调试信息')
-    # logging.info('输入详细信息')
-    # logging.warning('输入警告信息')
-    # logging.error('输入错误信息')
-    #
-    # # 使用current_app进行输出可控
-    # current_app.logger.debug('输入调试信息')
-    # current_app.logger.info('输入详细信息')
-    # current_app.logger.warning('输入警告信息')
-    # current_app.logger.error('输入错误信息')
+    # 3.拼接用户数据，渲染页面
+    data = {
+        "user_info":user.to_dict() if user else None
+    }
+    return render_template("news/index.html",data=data)
 
-    return render_template("news/index.html")
 
 # 处理网站logo
 @index_blue.route('/favicon.ico')
